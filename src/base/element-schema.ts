9b27fd2f4e17e6428790bfec7ac57d77f9374f2d
Khongchai <world1955@hotmail.com>
2026-09-01 16:58:08 +0700

    updated schema

--- src/base/element-schema.ts
+++ src/base/element-schema.ts
@@ -18,9 +18,33 @@ export type SubscribeableProperties = {
 
 export type SubscribeableKey = keyof SubscribeableProperties;
 
+export type AnimatedProp<T> = T[];
+
+export type LocalAnimationTransform = Partial<{
+    x: AnimatedProp<number>;
+    y: AnimatedProp<number>;
+    width: AnimatedProp<number>;
+    height: AnimatedProp<number>;
+    rotation: AnimatedProp<number>;
+    opacity: AnimatedProp<number>;
+}>;
+
+export type LocalAnimation = {
+    time: {
+        durationMs: number;
+        curve: "linear" | "ease-in-out";
+        repeat: "none" | "ping-pong";
+    };
+    animation: {
+        transform: LocalAnimationTransform;
+        curve: "linear" | "catmull";
+    };
+};
+
 export type UpdateableProperties = Partial<{
     meta: Record<string, unknown>;
     locked: boolean;
+    localAnimation: LocalAnimation | null;
 }>;
 
 export type SubscribedDrawdyElement = DrawdyElement &
